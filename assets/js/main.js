61db432ce0b86bbfeda3480238c4814c2c2976fc
alimovemil <alimov96emill@gmail.com>
2024-04-21 15:43:25 +0500

    fixed: swiper, autoplay

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,9 @@ let swiperproduct = new Swiper('.i-product', {
     centeredSlidesBounds: true,
     speed: 800,
     loop: true,
+    autoplay: {
+        delay: 5000,
+    },
     navigation: {
         prevEl: '.i-product .i-page.next',
         nextEl: '.i-product .i-page.prev',
@@ -314,9 +317,9 @@ let swiperbanner = new Swiper('.banner-item', {
     centeredSlidesBounds: true,
     speed: 800,
     loop: true,
-    // autoplay: {
-    //     delay: 5000,
-    // },
+    autoplay: {
+        delay: 5000,
+    },
     navigation: {
         prevEl: '.banner-item .i-page.next',
         nextEl: '.banner-item .i-page.prev',
